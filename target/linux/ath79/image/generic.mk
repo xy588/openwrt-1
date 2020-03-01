jimstar06/openwrt-LGA1150

ath79: add CSAC

target/linux/ath79/image/generic.mk
@@ -990,6 +990,15 @@ define Device/xiaomi_mi-router-4q
 endef
 TARGET_DEVICES += xiaomi_mi-router-4q
 
+define Device/xwrt_csac
+  ATH_SOC := qca9563
+  DEVICE_VENDOR := XWRT
+  DEVICE_MODEL := CSAC
+  IMAGE_SIZE := 16000k
+  DEVICE_PACKAGES := kmod-ath10k-ct ath10k-firmware-qca9888-ct kmod-usb2
+endef
+TARGET_DEVICES += xwrt_csac
+
 define Device/yuncore_a770
   ATH_SOC := qca9531
   DEVICE_VENDOR := YunCore

xy588/openwrt-1

target/linux/ath79/image/generic.mk
@@ -1170,6 +1170,15 @@ define Device/xiaomi_mi-router-4q
 endef
 TARGET_DEVICES += xiaomi_mi-router-4q
 
+define Device/xwrt_csac
+  SOC := qca9563
+  DEVICE_VENDOR := XWRT
+  DEVICE_MODEL := CSAC
+  IMAGE_SIZE := 16000k
+  DEVICE_PACKAGES := kmod-ath10k-ct ath10k-firmware-qca9888-ct kmod-usb2
+endef
+TARGET_DEVICES += xwrt_csac
+
 define Device/yuncore_a770
   SOC := qca9531
   DEVICE_VENDOR := YunCore